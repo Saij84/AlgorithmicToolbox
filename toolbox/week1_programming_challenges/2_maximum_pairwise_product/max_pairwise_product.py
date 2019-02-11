ebc72452cab9cdb67589052eff0f51650b5b390a
fangc <saij84@gmail.com>
2019-02-11 19:08:19 +1100

    check pairwise fast version

--- toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -3,16 +3,27 @@
 
 def max_pairwise_product(numbers):
     n = len(numbers)
-    max_product = 0
-    for first in range(n):
-        for second in range(first + 1, n):
-            max_product = max(max_product,
-                numbers[first] * numbers[second])
+    index1 = 1
+    index2 = 1
 
-    return max_product
+    for i in range(1, n):
+        if numbers[i] > numbers[0]:
+            index1 = numbers[i]
+
+    if index1 == 1:
+        index2 = 2
+    else:
+        index2 = 1
+
+    for i in range(1, n):
+        if numbers[i] != index1 and numbers[i] > index2:
+            index2 = numbers[i]
+
+    return index1*index2
 
 
 if __name__ == '__main__':
-    input_n = int(input())
-    input_numbers = [int(x) for x in input().split()]
+    input_var = [5,6,2,7,4,9]
+    #input_n = int(input_var)
+    input_numbers = [int(x) for x in input_var]
     print(max_pairwise_product(input_numbers))
