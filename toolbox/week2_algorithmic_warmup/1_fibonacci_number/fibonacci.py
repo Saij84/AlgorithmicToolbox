2d6aacf90802380417bf853d1465e53a599dc1c0
fangc <saij84@gmail.com>
2019-02-15 21:51:36 +1100

    solved fibonacci

--- toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -3,7 +3,7 @@ def calc_fib(n):
     if (n <= 1):
         return n
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
+    return n + calc_fib(n - 1)
 
 n = int(input())
 print(calc_fib(n))
---
+++ toolbox/week2_algorithmic_warmup/1_fibonacci_number/local_test.py
@@ -0,0 +1,24 @@
+def calc_fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    idx1 = 0
+    idx2 = 1
+    result = 0
+
+    i = 2
+
+    while i < n+1:
+        if i == 2:
+            result = idx1 + idx2
+        elif i == 3:
+            result = idx2 + result
+        else:
+            idx1 = idx2
+            idx2 = result
+            result = idx1 + idx2
+        i += 1
+    return result
+print(calc_fib())
