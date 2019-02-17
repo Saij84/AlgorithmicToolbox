f0bea8f9dcab87010750fe90d84147a8b6ce4dfa
fangc <saij84@gmail.com>
2019-02-18 07:31:08 +1100

    06 sum last digit solved

--- toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
+++ toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
@@ -1,4 +1,5 @@
 # Uses python3
+import time
 
 def get_pisano_period(m):
     val1 = 0
@@ -15,30 +16,6 @@ def get_pisano_period(m):
             return seq_len
 
 
-def calc_fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-
-    idx1 = 0
-    idx2 = 1
-    result = idx1 + idx2
-
-    i = 2
-
-    while i < n+1:
-        if i == 2:
-            result = idx1 + idx2
-        elif i == 3:
-            result = idx2 + result
-        else:
-            idx1, idx2 = idx2, result
-            result = (idx1 + idx2)
-        i += 1
-    return result
-
-
 def fibonacci_sum_naive(n):
     if n <= 1:
         return n
@@ -47,20 +24,45 @@ def fibonacci_sum_naive(n):
     current  = 1
     sum      = 1
 
-    #if n > 60:
-
-    for _ in range((n)-1):
+    for _ in range(n - 1):
         previous, current = current, previous + current
         sum += current
+
     return sum % 10
 
+def fibonacci_sum_fast(n, m):
+    if n <= 1:
+        return n
+
+    previous = 0
+    current  = 1
+    sum      = 1
+
+    remainder = n % get_pisano_period(m)
+
+    for _ in range(remainder-1):
+        previous, current = current, previous + current
+        sum += current
+
+    return sum % 10
+
+
 if __name__ == '__main__':
-    input = "10000"
-    n = int(input)
+    input = "8374293856215619875 10"
+    n, m = map(int, input.split())
 
-    print(fibonacci_sum_naive(n))
-    print(calc_fib(n))
+    # t0 = time.time()
+    # print(fibonacci_sum_naive(n))
+    # t1 = time.time()
+    # print("Slow time: {}".format(t1-t0))
 
-    if calc_fib(n) % 10 == fibonacci_sum_naive(n):
-        print("OK")
-    #print(fibonacci_sum_naive(n))
+    t0 = time.time()
+    print (fibonacci_sum_fast(n, m))
+    t1 = time.time()
+    print("fast time: {}".format(t1 - t0))
+
+"""
+00 01 02 03 04 05 06 07 08 09 
+00 01 01 02 03 05 08 13 21 34
+00 01 02 04 07 13 21 34 55 89
+"""
