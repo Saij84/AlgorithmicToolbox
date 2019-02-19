0c6e90d2cd2824602d2e1c9a707ee9146614e003
fangc <saij84@gmail.com>
2019-02-19 18:28:41 +1100

    08 complete

--- toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
+++ toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
@@ -12,9 +12,9 @@ def fibonacci_partial_sum_naive(from_, to):
             sum += current
 
         current, next = next, current + next
-        print(i, sum%10)
+        print(i, sum**2%10)
 
-    return sum % 10
+    return sum**2 % 10
 
 
 def fibonacci_partial_sum_fast(from_, to):
@@ -32,18 +32,19 @@ def fibonacci_partial_sum_fast(from_, to):
             sum += current
 
         current, next = next, current + next
+        print(i, sum**2 % 10)
 
-    return sum % 10
+    return sum**2 % 10
 
 
 if __name__ == '__main__':
-    input = "423521 1123155346456033"
+    input = "0 100"
     from_, to = map(int, input.split())
 
-    # t0 = time.time()
-    # print(fibonacci_partial_sum_naive(from_, to))
-    # t1 = time.time()
-    # print("time slow: {}".format(t1-t0))
+    t0 = time.time()
+    print(fibonacci_partial_sum_naive(from_, to))
+    t1 = time.time()
+    print("time slow: {}".format(t1-t0))
 
     t0 = time.time()
     print(fibonacci_partial_sum_fast(from_, to))
