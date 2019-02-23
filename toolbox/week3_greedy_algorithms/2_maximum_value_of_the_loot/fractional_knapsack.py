ad5d55e795f5eb26fab704f6e019c4e7f739cfd2
fangc <saij84@gmail.com>
2019-02-23 22:10:55 +1100

    02 complete

--- toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -1,10 +1,33 @@
 # Uses python3
 import sys
 
+# Sort values based on value/weight
+def sort_decending(weights, values):
+    weight_val_list = zip(weights, values)
+
+    sorted_dict = {}
+    for w, v in weight_val_list:
+        sorted_dict.update({v/w: (w, v)})
+
+    sorted_weights_values = [sorted_dict[key] for key in sorted(sorted_dict, reverse=True)]  # value, descending order
+
+    return sorted_weights_values
+
 def get_optimal_value(capacity, weights, values):
     value = 0.
-    # write your code here
 
+    sorted_weights_values = sort_decending(weights, values)
+
+    for items in sorted_weights_values:
+        if capacity == 0:
+            return value
+
+        if capacity >= items[0] and capacity != 0:
+            capacity = capacity - items[0]
+            value += items[1]
+        else:
+            value += items[1]/(items[0]/capacity)
+            capacity = 0
     return value
 
 
