b8921af37512125459d0fb495faa12916c059da2
fangc <saij84@gmail.com>
2019-02-23 18:18:14 +1100

    add money exchanger

--- toolbox/week3_greedy_algorithms/1_money_change/change.py
+++ toolbox/week3_greedy_algorithms/1_money_change/change.py
@@ -2,9 +2,17 @@
 import sys
 
 def get_change(m):
-    #write your code here
-    return m
+    denominations = [10, 5, 1]
+
+    change = 0
+
+    for deno in denominations:
+
+        remain = m//deno
+        change += remain
+        m = m - (remain*deno)
+    return change
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    m = int(28)
     print(get_change(m))
