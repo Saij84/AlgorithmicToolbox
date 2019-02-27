cab4d89bb0efc461785be6077bc163a52bad900f
fangc <saij84@gmail.com>
2019-02-27 12:53:00 +1100

    update

--- toolbox/week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ toolbox/week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -2,28 +2,32 @@
 import sys
 
 
-def compute_min_refills(distance, tank, stops):
+def compute_min_refills(distance, tank, num_stops, stops):
     num_refills = 0
     current_refills = 0
-    stops = [200, 375, 550, 750]
-    num_stops = len(stops)-1
+
+    if num_stops == 2 and distance < tank:
+        return 0
+
 
     while current_refills <= num_stops:
-        print(num_refills)
-        print("tdist:", stops[num_refills] + tank)
         last_refill = current_refills
+        if stops[last_refill] + tank >= distance:
+            return num_refills
+
+        print(stops[current_refills+1] - stops[last_refill])
 
         while current_refills < num_stops and (stops[current_refills+1] - stops[last_refill]) <= tank:
+            #print(stops[current_refills+1] - stops[last_refill])
             current_refills += 1
 
         if current_refills == last_refill:
             return -1
 
         if current_refills <= num_stops:
-            num_refills += num_refills
-
+            num_refills += 1
     return num_refills
 
 if __name__ == '__main__':
-    d, m, _, *stops = map(int, sys.stdin.read().split())
-    print(compute_min_refills(d, m, stops))
+    d, m, n, *stops = map(int, sys.stdin.read().split())
+    print(compute_min_refills(d, m, n, stops))
