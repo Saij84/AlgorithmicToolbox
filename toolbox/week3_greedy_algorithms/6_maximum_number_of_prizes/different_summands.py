415bc6c3c522a6a105d410a475af099d101c97ce
fangc <saij84@gmail.com>
2019-03-02 21:48:40 +1100

    06 solved

--- toolbox/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
+++ toolbox/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
@@ -3,7 +3,17 @@ import sys
 
 def optimal_summands(n):
     summands = []
-    #write your code here
+    k = n
+    index = 1  # starting number
+
+    while k > 0:  # run until k is equal to zero
+        if k <= 2 * index:  # while k decrease and index increase, trigger when smaller or equal to
+            summands.append(k)
+            k -= k  # set k to zero
+        else:
+            summands.append(index)  # push index to back of summands list
+            k -= index  # decrease k by index
+        index += 1  # increase index by one
     return summands
 
 if __name__ == '__main__':
