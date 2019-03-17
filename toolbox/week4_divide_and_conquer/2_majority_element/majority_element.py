0edf2de3160f5d1f31c90e3b8e5e981f5e26e6e6
fangc <saij84@gmail.com>
2019-03-17 14:47:23 +1100

    code cleanup to be more readeable

--- toolbox/week4_divide_and_conquer/2_majority_element/majority_element.py
+++ toolbox/week4_divide_and_conquer/2_majority_element/majority_element.py
@@ -3,34 +3,33 @@ import sys
 
 # go through an array of none negative numbers, return majority number, else return number
 def get_candidate(arr):
-    counter, possible_element = 0, None
-    for i in arr:
+    counter, candidate = 0, None  # initialise variables
+    for num in arr:
         if counter == 0:
-            possible_element, counter = i, 1
-        elif i == possible_element:
+            candidate, counter = num, 1  # new var assignment if counter is 0
+        elif num == candidate:
             counter += 1
         else:
             counter -= 1
-    return possible_element
+    return candidate
 
 # check if the number return from get candidate is an majority number
-def get_majority_element(a):
-    candidate = get_candidate(a)
-    count = 0
-
-    for i in a:
+def get_majority_element(arr):
+    count, candidate = 0, get_candidate(arr)
+    for i in arr:
         if i == candidate:
             count += 1
 
-    if count > len(a)/2:
+    if count > len(arr)/2:  # if the count is more than 50% of the total array
         return count
     else:
         return -1
 
 if __name__ == '__main__':
     input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
-    if get_majority_element(a) != -1:
-        print(1)
+    n, *arr = list(map(int, input.split()))
+    result = get_majority_element(arr)
+    if result != -1:
+        print(result, 1)
     else:
         print(0)
