975864718525162f046064f0ff2e6c45097b3a85
fangc <saij84@gmail.com>
2019-03-21 07:41:29 +1100

    quick sort prototype

--- toolbox/week4_divide_and_conquer/3_improving_quicksort/_tests.py
+++ toolbox/week4_divide_and_conquer/3_improving_quicksort/_tests.py
@@ -1,5 +1,8 @@
 from utils.src.array_utils import array_utils as aUtils
 from utils.src.time_utils import time_utils as tUtils
+from utils.src.sorting_utils import sorting_utils as sUtils
+
+import random
 
 @tUtils.function_timer
 def selection_sort(arr):
@@ -11,8 +14,22 @@ def selection_sort(arr):
                 arr[j], arr[i] = arr[i], arr[j]
     return arr
 
+def sorting(arr):
+    array_len = len(arr)
+    left, right = [], []
 
+    pivot = arr[random.randint(0, array_len-1)]
+    print(pivot)
 
+    for i in arr:
+        if i <= pivot:
+            left.append(i)
+        else:
+            right.append(i)
+
+    l_sorted = sUtils.merge_sort(left)
+    r_sorted = sUtils.merge_sort(right)
+    return l_sorted + r_sorted
 
 def count_sort(arr):
     count = []
@@ -24,5 +41,5 @@ def count_sort(arr):
 for i in range(1):
     data_in = aUtils.array_generator(1,9, isRandomRange=False)
     print(data_in)
-    print(merge_sort(data_in))
+    print(sorting(data_in))
 
