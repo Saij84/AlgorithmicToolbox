569d5841e1e7e8303db0e9f1197627b58aeb4daa
fangc <saij84@gmail.com>
2019-03-26 21:29:11 +1100

    imporving quicksort complete

--- toolbox/week4_divide_and_conquer/3_improving_quicksort/sorting.py
+++ toolbox/week4_divide_and_conquer/3_improving_quicksort/sorting.py
@@ -2,9 +2,10 @@
 import sys
 import random
 from utils.src.array_utils import array_utils as aUtils
+from utils.src.checking_utils import checking_utils as cUtils
 
 
-def partition3_1(a, l, r):
+def partition3(a, l, r):
     pivot, l_idx, r_idx = a[l], l, r
     idx = l_idx
 
@@ -20,46 +21,25 @@ def partition3_1(a, l, r):
         idx += 1
     return l_idx, r_idx
 
-
-def partition3(a, l, r):
-    x, j, t = a[l], l, r
-    i = j
-
-    while i <= t:
-        if a[i] < x:
-            a[j], a[i] = a[i], a[j]
-            j += 1
-
-        elif a[i] > x:
-            a[t], a[i] = a[i], a[t]
-            t -= 1
-            i -= 1  # remain in the same i in this case
-        i += 1
-    return j, t
-
-
-
 def randomized_quick_sort(a, l, r):
     if l >= r:
         return
     k = random.randint(l, r)
     a[l], a[k] = a[k], a[l]
     m1, m2 = partition3(a, l, r)
-    m3, m4 = partition3_1(a, l, r)
-    print("orig:", m1, m2)
-    print(" new:", m3, m4)
 
-
-    #
-    # randomized_quick_sort(a, l, m1)
-    # randomized_quick_sort(a, m2 + 1, r)
-    # return a
+    randomized_quick_sort(a, l, m1)
+    randomized_quick_sort(a, m2 + 1, r)
+    return a
 
 
 if __name__ == '__main__':
-    input = [8, 9, 4, 6, 7, 10, 6, 7, 4, 4]  # aUtils.array_generator(1, 10, arr_size=10, isRandomRange=False)
-    n, a = len(input), input
-    print(input)
-    randomized_quick_sort(a, 0, n - 1)
-    for x in a:
-        print(x, end=' ')
+    for i in range(10):
+        input = aUtils.array_generator(1, 500, arr_size=25, isRandomRange=True)
+        n, a = len(input), input
+        print(input)
+        result = randomized_quick_sort(a, 0, n - 1)
+        print(cUtils.growing_num(result))
+        for x in a:
+            print(x, end=' ')
+        print("\n")
