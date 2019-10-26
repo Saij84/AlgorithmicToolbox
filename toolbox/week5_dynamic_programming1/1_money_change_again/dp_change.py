3201aec4dba1081b90142ce9ce74ff75972bd267
Fang-min Chen <saij84@gmail.com>
2019-10-26 16:55:03 +1100

    update

--- toolbox/week5_dynamic_programming1/1_money_change_again/dp_change.py
+++ toolbox/week5_dynamic_programming1/1_money_change_again/dp_change.py
@@ -8,13 +8,6 @@ def DPChange(money, coins):
         min_num_coins[m] = inf
         for i in coins:
             if m >= i:
-                print("min_num_coins[m - i] + 1 < min_num_coins[m]")
-                print("-"*50)
-                print("min_num_coins[m - i]+1:",  (m, i), min_num_coins[m - i]+1)
-                print("min_num_coins[m]:", (m), min_num_coins[m])
-                print(min_num_coins[m - i] + 1 < min_num_coins[m])
-                print("\n")
-
                 if min_num_coins[m - i] + 1 < min_num_coins[m]:
                     min_num_coins[m] = min_num_coins[m - i] + 1
     return min_num_coins[money]
